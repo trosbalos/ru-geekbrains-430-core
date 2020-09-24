f9a8e87bc534da0ec5ebbbcd858562baa921316b
trosbalos <62149688+trosbalos@users.noreply.github.com>
2020-09-25 00:19:57 +0300

    Тесты 2 и 3 готовы

--- src/main/java/hw2/ArraySorter.java
+++ src/main/java/hw2/ArraySorter.java
@@ -10,28 +10,29 @@ public class ArraySorter {
         if (arr.length != 4) {
             throw new MyArraySizeException("Размер массива не 4 на 4");
         }
+
+
+
         int sum = 0;
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-
-                sum += Integer.parseInt(arr[i][j]);
-                System.out.println( i + ""+ j);
                 // Ловим MyArrayDataException
                 try {
-                    int s = Integer.parseInt(arr[i][j]);
+                    Integer.parseInt(arr[i][j]);
                 } catch (NumberFormatException e) {
                     throw new MyArrayDataException(
                             "Ячейка массива " + i + " " + j + " содержит не число");
                 }
-                return sum;
+
+
+                sum += Integer.parseInt(arr[i][j]);
 
 
             }
+     }
 
-        }
         System.out.println("Сумма чисел массива равна " + sum);
         return sum;
     }
-
 }
--- src/test/java/hw2/ArraySorterTest.java
+++ src/test/java/hw2/ArraySorterTest.java
@@ -15,26 +15,10 @@ public class ArraySorterTest {
                 testarr[i][j] = String.valueOf(i + j);
             }
         }
-//        testarr[0][0] = "1";
-//        testarr[0][1] = "2";
-//        testarr[0][2] = "3";
-//        testarr[0][3] = "4";
-//        testarr[1][0] = "5";
-//        testarr[1][1] = "6";
-//        testarr[1][2] = "7";
-//        testarr[1][3] = "8";
-//        testarr[2][0] = "9";
-//        testarr[2][1] = "10";
-//        testarr[2][2] = "11";
-//        testarr[2][3] = "12";
-//        testarr[3][0] = "13";
-//        testarr[3][1] = "14";
-//        testarr[3][2] = "15";
-//        testarr[3][3] = "16";
 
        int actual = arraySorter.arrSorter(testarr);
 
-        Assert.assertEquals(9, actual);
+        Assert.assertEquals(48, actual);
     }
     
 
